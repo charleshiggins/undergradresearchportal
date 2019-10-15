febbfcbd7193b483ca87976f1651411a73abb7eb
reesb <10052971+reesb@users.noreply.github.com>
2019-10-14 22:41:27 -0400

    adding header

---
+++ client/src/components/Unauth.js
@@ -0,0 +1,12 @@
+
+
+export default class Unauth extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <div>Unauth</div>
+            </div>
+        );
+    }
+}
