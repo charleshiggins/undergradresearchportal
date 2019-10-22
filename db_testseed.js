20563f3e7b130f03abc82b140e564ffae68cda7a
reesb <10052971+reesb@users.noreply.github.com>
2019-10-22 11:54:37 -0400

    now uploads filenames to database. not files, just filenames for now

--- db_testseed.js
+++ db_testseed.js
@@ -11,15 +11,15 @@ con.connect();
 
 console.log("Connected!");
 
- con.query("DROP DATABASE IF EXISTS testdb", function (err, result) {
-   if (err) throw err;
-   console.log("Database dropped.");
- });
+con.query("DROP DATABASE IF EXISTS testdb", function (err, result) {
+  if (err) throw err;
+  console.log("Database dropped.");
+});
 con.query("CREATE DATABASE IF NOT EXISTS testdb", function (err, result) {
   if (err) throw err;
   console.log("Database created.");
 });
-con.query("CREATE TABLE IF NOT EXISTS testdb.application (id INTEGER PRIMARY KEY, fname varchar(255),  mname varchar(255),  lname varchar(255), pid varchar(255), email varchar(255), facultyName varchar(500), facultyEmail varchar(255), academicDepartment varchar(255))", function (err, result) {
+con.query("CREATE TABLE IF NOT EXISTS testdb.application (id INTEGER PRIMARY KEY, fname varchar(255),  mname varchar(255),  lname varchar(255), pid varchar(255), email varchar(255), proposal varchar(255), transcript varchar(255), facultyName varchar(500), facultyEmail varchar(255), academicDepartment varchar(255))", function (err, result) {
   if (err) throw err;
   console.log("Table created.");
 });
--- static_views/submitApp.js
+++ static_views/submitApp.js
@@ -6,28 +6,30 @@ async function submitApp() {
             "lname": document.getElementById('lname').value,
             "pid": document.getElementById('pid').value,
             "email": document.getElementById('email').value,
+            "proposal": document.getElementById('proposal').value,
+            "transcript": document.getElementById('transcript').value,
             "Faculty": {
                 "fullname": document.getElementById('facultyName').value,
                 "email": document.getElementById('facultyEmail').value,
                 "academicDepartment": document.getElementById('facultyDepartment').value
             }
         }
-    
+
     }
     submitApplication(applicationObject).then(() => {
         location.replace("student_view_3.html")
-      });
+    });
 
 }
 
-async function submitApplication(applicationObject){
+async function submitApplication(applicationObject) {
     const response = await fetch('http://localhost:8080/submitApplication', {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-          },
+        },
         method: 'post',
-        body: JSON.stringify(applicationObject)});
-        console.log(response);
-
+        body: JSON.stringify(applicationObject)
+    });
+    console.log(response);
 }
