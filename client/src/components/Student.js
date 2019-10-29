9e6b7113f876ee4571956cff41959dcc9c3a58b6
Charles Higgins <charleshiggins2112@gmail.com>
2019-10-28 22:43:38 -0400

    slowly changing student view to react

--- client/src/components/Student.js
+++ client/src/components/Student.js
@@ -3,18 +3,52 @@ import React from 'react';
 import { Header } from './Header.js';
 import { submitApplication } from '../api.js'
 export class Unauth extends React.Component {
+    state = {
+        view = 1,
+        fname= "",
+        mname= "",
+        lname= "",
+    }
 
+    handleTextInputChange(event) {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    }
     submit() {
         submitApplication("test");
     }
 
     render() {
-        return (
-            <div>
-                <Header />
-                <div>Unauth</div>
-                <button onClick={this.submit}>Insert row into database</button>
+        return (<div>
+            <Header />
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="card" style="height: 100%; overflow: scroll;">
+                            <div class="card-body">
+                            <form class="form" role="form" autocomplete="on">
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label" for="projTitle">Project Title</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="projTitle" placeholder="" />
+                                </div>
+                            </div>
+
+                            </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="col-sm-2 float-right pr-0 py-5">
+                        <a style="width: 75%;" class="btn btn-primary btn-sm align-self-center float-right" onclick="nextPage()"
+                            role="button" target="_blank">Continue</a>
+                    </div>
+                </div>
             </div>
+        </div>
+
         );
     }
 }
