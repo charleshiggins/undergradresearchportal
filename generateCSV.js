921440fabd4193e38307deb15f5460df98ca187b
Charles Higgins <charleshiggins2112@gmail.com>
2019-10-29 02:29:21 -0400

    can generate csv file populated with application data

--- generateCSV.js
+++ generateCSV.js
@@ -1,20 +1,55 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const csvWriter = createCsvWriter({
-    path: 'output.csv',
-    header: [
-        { id: 'onyen', title: 'Onyen' },
-        { id: 'fname', title: 'First Name' }
-    ]
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "pass",
 });
 
-const applicationInfo = [
-    { onyen: 'charles7', fname: 'Charles' },
-    { onyen: 'reesb', fname: 'Rees' },
-    { onyen: 'annunzip', fname: 'Patrick' },
+queryResults = {}
 
-];
 
-csvWriter.writeRecords(applicationInfo)
-    .then(() => {
-        console.log("csv succesfully generated");
+con.connect(function (err) {
+    if (err) throw err;
+    con.query("SELECT * FROM testdb.application", function (err, result, fields) {
+        if (err) throw err;
+        queryResults = result;
+        makeCSV()
     });
+});
+
+
+function makeCSV() {
+    console.log(queryResults)
+
+
+    const csvWriter = createCsvWriter({
+        path: 'output.csv',
+        header: [
+            { id: 'fname', title: 'First Name' },
+            { id: 'mname', title: 'Middle Inital' },
+            { id: 'lname', title: 'Last Name' },
+            { id: 'pid', title: 'pid' },
+            { id: 'email', title: 'Email' },
+            { id: 'facultyName', title: 'Faculty Name' },
+            { id: 'facultyEmail', title: 'Faculty Email' },
+            { id: 'academicDepartment', title: 'Academic Department' },
+
+
+        ]
+    });
+    const applicationInfo = []
+    for (i = 0; i < queryResults.length; i++) {
+        applicationInfo.push({
+            fname: queryResults[i].fname, mname: queryResults[i].mname, lname: queryResults[i].lname, pid: queryResults[i].pid,
+            email: queryResults[i].email, facultyName: queryResults[i].facultyName, facultyEmail: queryResults[i].facultyEmail,
+            academicDepartment: queryResults[i].academicDepartment
+        })
+    }
+
+    csvWriter.writeRecords(applicationInfo)
+        .then(() => {
+            console.log("csv succesfully generated");
+        });
+}
